60d389f31c94030c3af32eff29fa103a0cf96f1c
Unknown <GKelly446@west-mec.org>
2018-08-08 15:24:57 -0700

    finished function to append child nodes to change dates for months

--- snoot.js
+++ snoot.js
@@ -16,7 +16,6 @@ var thirtyOne = document.createDocumentFragment();
 // function to remove select list defaults
 function removeSelectDefaults() {
     var emptyBoxes  = document.getElementsByTagName("select");
-    alert("select lists: " + emptyBoxes.length);
     for (var i = 0; i < emptyBoxes.length; i++) {
         emptyBoxes[i].selectedIndex = -1;
     }
@@ -43,19 +42,58 @@ function updateDays() {
    while (dates[28]) {
         deliveryDay.removeChild(dates[28]);
     }
+    if (deliveryYear.selectedIndex === -1) {
+        deliveryDay.selectedIndex = 0; 
+    }
+    
+    // if it's feburary and the year 2020, twentyNine
+    if (selectedMonth === "2" && deliveryYear.options[deliveryYear.selectedIndex].value === "2020") {
+        deliveryDay.appendChild(twentyNine.cloneNode(true));
+    }   
+    // else if it's a 30 day month, thirty
+    else if (selectedMonth === "4" || selectedMonth === "6" || selectedMonth === "9" || selectedMonth === "11") {
+        deliveryDay.appendChild(thirty.cloneNode(true));
+
+    }
+    // else if 31 day month, thirtyOne
+    else if (selectedMonth === "1" || selectedMonth === "3" || selectedMonth === "5" || selectedMonth === "7" || selectedMonth === "8" || selectedMonth === "10" || selectedMonth === "12") {
+        deliveryDay.appendChild(thirtyOne.cloneNode(true));
+
+    }
 }
 
 // function that sets up page on load event 
 function setUpPage() {
     removeSelectDefaults();
     setUpDays();
-    updateDays();
+    createEventListeners();
 }
 
+// function to create our event listeners 
+
+    //if the month changes
+function createEventListeners() {
+    var deliveryMonth = document.getElementById("delivMo");
+        if (deliveryMonth.addEventListener) {
+            deliveryMonth.addEventListener("change", updateDays, false);
+        } else if (deliveryMonth.attachEvent) {
+            deliveryMonth.attachEvents("onchange", updateDays);
+        }   
+    
+    //if the year changes
+     var deliveryYear = document.getElementById("delivYr");
+        if (deliveryYear.addEventListener) {
+            deliveryYear.addEventListener("change", updateDays, false);
+        } else if (deliveryYear.attachEvent) {
+            deliveryYear.attachEvents("onchange", updateDays);
+        }   
+}
+    
 //page load event handlers
 if (window.addEventListener) {
     window.addEventListener("load", setUpPage, false);
-} else if (window.attatchEvent) {
-    window.attatchEvents("onload", setUpPage);
+} else if (window.attachEvent) {
+    window.attachEvents("onload", setUpPage);
      
 }
+
